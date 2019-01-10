4eca0b26faf45726e464b9680ca04fc6320faed6
toberi <toberi@home.se>
2019-01-10 16:25:03 +0100

    mappa till entity

--- Driftservice-Registration/Models/ViewModel.cs
+++ Driftservice-Registration/Models/ViewModel.cs
@@ -57,5 +57,80 @@ namespace Driftservice_Registration.Models
         public int ServiceTypeID { get; set; }
 
         public virtual ICollection<ServiceType> ServiceTypes { get; set; }
+
+        //public ServiceType MapVMToServiceType()
+        //{
+        //    return new ServiceType()
+        //    {
+        //        ServiceTypeID = ServiceTypeID
+        //    };
+        //}
+
+        //public Contact MapVMToContact()
+        //{
+        //    return new Contact()
+        //    {
+        //        ContactID = ContactID,
+        //        FirstName = FirstName,
+        //        LastName = LastName,
+        //        Business = Business,
+        //        Email = Email,
+        //        PhoneNumber = PhoneNumber,
+        //        NotificationType = NotificationType,
+        //        ContactGuid = ContactGuid,
+        //        Language = Language,
+        //        RegDate = RegDate,
+        //    };
+        //}
+
+        public void MapVMToModel(ref Contact contact, ServiceType serviceType)
+        {
+            contact.ContactID = ContactID;
+            contact.FirstName = FirstName;
+            contact.LastName = LastName;
+            contact.Business = Business;
+            contact.Email = Email;
+            contact.PhoneNumber = PhoneNumber;
+            contact.NotificationType = NotificationType;
+            contact.ContactGuid = ContactGuid;
+            contact.Language = Language;
+            contact.RegDate = RegDate;
+            serviceType.ServiceTypeID = ServiceTypeID;
+        }DBNull.save
+
+        public static implicit operator ViewModel(Contact contact)
+        {
+            return new ViewModel()
+            {
+                ContactID = contact.ContactID,
+                FirstName = contact.FirstName,
+                LastName = contact.LastName,
+                Business = contact.Business,
+                Email = contact.Email,
+                PhoneNumber = contact.PhoneNumber,
+                NotificationType = contact.NotificationType,
+                ContactGuid = contact.ContactGuid,
+                Language = contact.Language,
+                RegDate = contact.RegDate
+            };
+        }
+
+        public static implicit operator Contact(ViewModel viewModel)
+        {
+            return new Contact()
+            {
+                ContactID = viewModel.ContactID,
+                FirstName = viewModel.FirstName,
+                LastName = viewModel.LastName,
+                Business = viewModel.Business,
+                Email = viewModel.Email,
+                PhoneNumber = viewModel.PhoneNumber,
+                NotificationType = viewModel.NotificationType,
+                ContactGuid = viewModel.ContactGuid,
+                Language = viewModel.Language,
+                RegDate = viewModel.RegDate
+            };
+        }
+
     }
 }
